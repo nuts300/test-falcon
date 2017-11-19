1d6edb2ce16915c4a2a7f23ca86c20d34069be68
Unknown <nuts300@gmail.com>
2017-11-20 00:12:11 +0900

    Refacor de_operator

--- sample/db_operator/main.py
+++ sample/db_operator/main.py
@@ -1,10 +1,11 @@
 import falcon
 from mongoengine.errors import ValidationError
-from sample.db_operators.documents.user import UserDocument
+from sample.db_operator.documents.user import UserDocument
 from sample.error.sample_error import SampleError
 from sample.error.code import ErrorCode
 
-class UsersOperator(object):
+class DbOperator(object):
+    @staticmethod
     def createUser(userPayload):
         try:
             user = UserDocument(**userPayload)
@@ -14,11 +15,13 @@ class UsersOperator(object):
             raise SampleError(status=falcon.HTTP_400, code=ErrorCode.INVALID_USER, exception=err, vars=userPayload)
         except Exception as err:
             raise SampleError(status=falcon.HTTP_500, code=ErrorCode.FAILED_CREATE_USER, exception=err, vars=userPayload)
-        
+    
+    @staticmethod
     def getUser(id):
         user = UserDocument.objects(id=ObjectId(id))
         return user.to_json()
 
+    @staticmethod
     def getUsers():
         users =UserDocument.objects.all()
         return users.to_json()
--- sample/resources/users.py
+++ sample/resources/users.py
@@ -1,20 +1,20 @@
 import falcon
 from bson.objectid import ObjectId
-from sample.db_operators.users import UsersOperator
+from sample.db_operator.main import DbOperator
 from sample.extentions.parse_json import parseJson
 
 class User(object):
     def on_get(self, req, resp, id):
-        result = UsersOperator.getUser(id)
+        result = DbOperator.getUser(id)
         resp.body = result
 
 class Users(object):
     def on_get(self, req, resp):
-        result = UsersOperator.getUsers()
+        result = DbOperator.getUsers()
         resp.body = result
 
     @falcon.before(parseJson)
     def on_post(self, req, resp):
         user = req.body
-        result = UsersOperator.createUser(user)
+        result = DbOperator.createUser(user)
         resp.body = result
