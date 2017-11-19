0f970e8e4739a25a1554e19e4921d48613856c87
Unknown <nuts300@gmail.com>
2017-11-19 19:56:27 +0900

    Add vars to SampleError

--- sample/db_operators/users.py
+++ sample/db_operators/users.py
@@ -5,15 +5,15 @@ from sample.error.sample_error import SampleError
 from sample.error.code import ErrorCode
 
 class UsersOperator(object):
-    def createUser(user):
+    def createUser(userPayload):
         try:
-            user = UserDocument(**user)
+            user = UserDocument(**userPayload)
             user.save()
             return user.to_json()
         except ValidationError as err:
-            raise SampleError(status=falcon.HTTP_400, code=ErrorCode.INVALID_USER.name, message=ErrorCode.INVALID_USER.value, exception=err)
+            raise SampleError(status=falcon.HTTP_400, code=ErrorCode.INVALID_USER.name, message=ErrorCode.INVALID_USER.value, exception=err, vars=userPayload)
         except Exception as err:
-            raise SampleError(status=falcon.HTTP_500, code=ErrorCode.FAILED_CREATE_USER.name, message=ErrorCode.FAILED_CREATE_USER.value, exception=err)
+            raise SampleError(status=falcon.HTTP_500, code=ErrorCode.FAILED_CREATE_USER.name, message=ErrorCode.FAILED_CREATE_USER.value, exception=err, vars=userPayload)
         
     def getUser(id):
         user = UserDocument.objects(id=ObjectId(id))
--- sample/error/sample_error.py
+++ sample/error/sample_error.py
@@ -7,14 +7,15 @@ from sample.logger import getLogger
 logger = getLogger(__name__)
 
 class SampleError(Exception):
-    def __init__(self, exception, status, code, message):
+    def __init__(self, exception, status, code, message, vars):
         self.exception = exception
         self.status = status
         self.code = code
         self.message = message
+        self.vars = vars
 
     @staticmethod
     def handle(ex, req, resp, params):
-        logger.error("[code] %s [message] %s\n%s" % (ex.code, ex.message, traceback.format_exc()))
+        logger.error("[code] %s [message] %s %s\n%s" % (ex.code, ex.message, ex.vars, traceback.format_exc()))
         raise falcon.HTTPError(ex.status, code=ex.code, title=ex.message, description=traceback.format_exc())
     
