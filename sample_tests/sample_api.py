2149c94ea445d4d8aabb5b6133e6be21041ced57
nuts <nuts300@gmail.com>
2017-11-25 17:06:26 +0900

    Add test for create user

--- sample_tests/sample_api.py
+++ sample_tests/sample_api.py
@@ -1,3 +1,4 @@
+import json
 from falcon import testing
 from sample.app import get_app
 
@@ -12,7 +13,34 @@ class MyTestCase(testing.TestCase):
         self.app = get_app()
 
 
+# pylint: disable=W0212,W0201
 class TestMyApp(MyTestCase):
+
     def test_hello(self):
-        result = self.simulate_get('/hello')
-        self.assertEqual(result._content, b"hello sample service")
+        response = self.simulate_get('/hello')
+        self.assertEqual(response._content, b"hello sample service")
+
+    def test_create_user(self):
+        body = json.dumps({"name": "aaaaaa", "age":20})
+        response = self.simulate_post(
+            '/users',
+            body=body,
+            headers={'content-type': 'application/json'}
+        )
+        content = json.loads(response._content)
+        created_user_id = content.get("_id").get("$oid")
+        self.assertEqual(response._status_code, 200)
+
+        print('/users/' + created_user_id)
+        get_resp = self.simulate_get('/users/' + created_user_id)
+        self.assertEqual(get_resp._status_code, 200)
+        print(get_resp.json)
+        self.assertEqual(get_resp.json.get("_id").get("$oid"), created_user_id)
+
+    # def test_get_user(self):
+    #     response = self.simulate_get('/users/' + self._created_user_id)
+    #     print(response._content)
+
+    # def test_get_users(self):
+    #     result = self.simulate_get('/users')
+    #     print(vars(result))
