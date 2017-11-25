6731dc23b2c0452478e048c95b4c1341c9bbc865
nuts <nuts300@gmail.com>
2017-11-26 00:47:54 +0900

    Add db host and db port to env

--- sample/app.py
+++ sample/app.py
@@ -7,9 +7,12 @@ from sample.resources.users import User, Users
 from sample.extentions.print_log_middleware import PrintLogMiddleware
 from sample.error.sample_error import SampleError
 from sample.error.final_error_handler import final_error_handler
+from sample.env import get_db_host, get_db_port
 
 # connect db
-connect('mongotest', host='127.0.0.1', port=3001)
+DB_HOST = get_db_host()
+DB_PORT = get_db_port()
+connect('mongotest', host=DB_HOST, port=DB_PORT)
 
 def get_app():
     api = falcon.API(middleware=[PrintLogMiddleware()])
--- sample/env.py
+++ sample/env.py
@@ -1,8 +1,27 @@
 import os
 
+DEFAULT_DB_HOST = "mongodb://localhost"
+DEFAULT_DB_PORT = 3001
+
 # pylint: disable=W0703
 def is_production() -> bool:
     try:
         return os.environ['SAMPLE_APP_PRODUCTION'] == "true"
-    except Exception:
+    except KeyError:
         return False
+
+def get_db_host() -> str:
+    try:
+        if os.environ["SAMPLE_APP_DB_HOST"]:
+            return os.environ["SAMPLE_APP_DB_HOST"]
+        return DEFAULT_DB_HOST
+    except KeyError:
+        return DEFAULT_DB_HOST
+
+def get_db_port() -> int:
+    try:
+        if os.environ["SAMPLE_APP_DB_PORT"]:
+            return int(os.environ["SAMPLE_APP_DB_PORT"])
+        return DEFAULT_DB_PORT
+    except KeyError:
+        return DEFAULT_DB_PORT
