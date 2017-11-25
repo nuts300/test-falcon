724eb7df4c0d72e57d929451dcdf0827c132692d
nuts <nuts300@gmail.com>
2017-11-25 18:45:44 +0900

    Add test for delete user

--- sample_tests/sample_api_test.py
+++ sample_tests/sample_api_test.py
@@ -1,4 +1,5 @@
 import json
+import unittest
 from falcon import testing
 from sample.app import get_app
 
@@ -13,14 +14,14 @@ class MyTestCase(testing.TestCase):
         self.app = get_app()
 
 
-# pylint: disable=W0212,W0201,C0301
+# pylint: disable=W0212,W0201,C0301,W0703
 class TestMyApp(MyTestCase):
 
-    def test_hello(self):
+    def test_01_hello(self):
         response = self.simulate_get('/hello')
         self.assertEqual(response._content, b"hello sample service")
 
-    def test_create_user(self):
+    def test_02_create_user(self):
         body = json.dumps({"name": "aaaaaa", "age":20})
         response = self.simulate_post(
             '/users',
@@ -30,13 +31,34 @@ class TestMyApp(MyTestCase):
         TestMyApp._created_user_id = response.json.get("_id").get("$oid")
         self.assertEqual(response._status_code, 200)
 
-    def test_get_user(self):
+    def test_03_get_user(self):
         response = self.simulate_get('/users/' + TestMyApp._created_user_id)
         self.assertEqual(response._status_code, 200)
         self.assertEqual(response.json.get("_id").get("$oid"), TestMyApp._created_user_id)
 
-    def test_get_users(self):
+    def test_04_get_users(self):
         response = self.simulate_get('/users')
         self.assertTrue(len(response.json) > 0)
         finded = [user for user in response.json if user.get("_id").get("$oid") == TestMyApp._created_user_id]
         self.assertTrue(len(finded) == 1)
+
+    def test_05_update_user(self):
+        response = self.simulate_put(
+            '/users/' + TestMyApp._created_user_id,
+            body=json.dumps({"name": "changed"}),
+            headers={'content-type': 'application/json'}
+        )
+        self.assertEqual(response._status_code, 204)
+
+        get_res = self.simulate_get('/users/' + TestMyApp._created_user_id)
+        self.assertEqual(get_res.json.get("name"), "changed")
+
+    def test_06_delete_user(self):
+        response = self.simulate_delete('/users/' + TestMyApp._created_user_id)
+        self.assertEqual(response._status_code, 204)
+
+        get_res = self.simulate_get('/users/' + TestMyApp._created_user_id)
+        self.assertEqual(get_res._status_code, 404)
+
+    if __name__ == '__main__':
+        unittest.main(failfast=True, exit=False)
