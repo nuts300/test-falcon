9ab0e4f743fc4b19982d7e316c7ddad21bb25dd9
nuts <nuts300@gmail.com>
2017-11-26 12:06:08 +0900

    Normalise result json

--- sample/db_operator/main.py
+++ sample/db_operator/main.py
@@ -2,28 +2,41 @@ from typing import Optional, List, Any
 import falcon
 from sample.db_operator.schemas.users import Users
 
+def normalize_mongo_id(byson_obj: dict) -> dict:
+    byson_obj["id"] = str(byson_obj["_id"])
+    del byson_obj["_id"]
+    return byson_obj
+
 # pylint: disable=E1101
 class DbOperator(object):
     @staticmethod
     def create_user(user_payload: dict) -> Optional[object]:
         user = Users(**user_payload)
         user.save()
-        return user.to_mongo()
+        mongo_obj = user.to_mongo()
+        return normalize_mongo_id(mongo_obj)
 
     @staticmethod
     def get_user(user_id: str) -> Optional[object]:
-        return Users.objects.with_id(user_id).to_mongo()
+        user = Users.objects.with_id(user_id)
+        if user:
+            mongo_obj = user.to_mongo()
+            return normalize_mongo_id(mongo_obj)
+        return None
 
     @staticmethod
     def get_users() -> List[Any]:
         users = Users.objects.all()
-        return [user.to_mongo() for user in users]
+        mongo_obj_array = [user.to_mongo() for user in users]
+        return [normalize_mongo_id(i) for i in mongo_obj_array]
 
     @staticmethod
     def update_user(user_id: str, user_payload: str) -> Optional[object]:
-        updated = Users.objects(id=user_id).update_one(**user_payload)
-        if updated > 0:
-            return Users.objects.with_id(user_id).to_mongo()
+        Users.objects(id=user_id).update_one(**user_payload)
+        user = Users.objects.with_id(user_id)
+        if user:
+            mongo_obj = user.to_mongo()
+            return normalize_mongo_id(mongo_obj)
         return None
 
     @staticmethod
